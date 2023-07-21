1cb63a69f32881c721e1e5b16ea0b83ee225d5fc
greg-pitlabs <greg@pitlabs.xyz>
2023-07-21 16:27:46 -0700

    Updated docs and sample

--- README.md
+++ README.md
@@ -10,6 +10,26 @@ with the same confidence and assurances as on internal artifacts.
 
 The following samples illustrate common solutions built on top of the C2 library and services.
 
-## Installation
+## Setup
 
+### Install the c2-py library
 
+- Clone the `c2-py` repository `https://github.com/pit-labs/c2-py.git`.
+  - `c2-py` is the Python library for interacting with the ChronoCloud (C2) environment. 
+  It is required by most command-line tools and data science workflows.
+  - At the time of this limited release, the repository is private. 
+  It can be cloned using GitHub Desktop or another authenticated solution.
+  - This guide assumes the repository has been cloned to the local path `~/c2/c2-py`.
+- Install the `c2py` Python package from the cloned repository:
+```commandline
+pip install ~/c2/c2-py
+```
+
+### Configure C2 access
+
+If you have previously configured C2 access, for instance when using the `c2-py-tools` package,
+you can re-use those settings by copying `.env` file to the `c2-py-samples` folder.
+If this is your first time working with C2, you should configure new settings.
+
+If this is your first time accessing C2, please install the `c2-py-tools` package 
+and follow the setup instructions using the `config_env` script provided in that package. 
--- samples/create_dataset.py
+++ samples/create_dataset.py
@@ -5,33 +5,34 @@ C2 dataset creation sample
 from dotenv import dotenv_values
 
 from c2 import (
-    # Class providing ChronoCloud (C2) operations.
     C2,
-    # Class providing a commitment service used by C2 operations.
-    Web3HTTPCommitmentService
+    Web3HTTPCommitmentService,
 )
 
-# Load environment variables.
+# Load environment variables containing C2 configuration.
 env_vars = dotenv_values(".env")
 
 # Create a C2 object using a Web3 HTTP commitment service.
 # The commitment service is a smart contract running on a blockchain.
 # Use connection parameters specified in environment variables.
-c2 = C2(Web3HTTPCommitmentService(
-    # RPC endpoint used to access the commitment service.
-    # This is an url for a blockchain RPC endpoint.
-    endpoint_url=env_vars["ENDPOINT_URL"],
-    # Address of the smart contract providing the commitment service.
-    c2c_address=env_vars["C2C_ADDRESS"],
-    # Private key used to access the commitment service.
-    # The private key defines the user address that owns the commitments
-    # and the corresponding C2 datasets and records.
-    private_key=env_vars["PRIVATE_KEY"]
-))
+c2 = C2(
+    Web3HTTPCommitmentService(
+        # RPC endpoint used to access the blockchain running the
+        # commitment service smart contract.
+        endpoint_url=env_vars["ENDPOINT_URL"],
+        # Address of the smart contract providing the commitment service.
+        c2c_address=env_vars["C2C_ADDRESS"],
+        # Private key used to access the commitment service.
+        # The private key defines the user address that owns the commitments
+        # and the corresponding C2 datasets and records.
+        private_key=env_vars["PRIVATE_KEY"],
+    )
+)
 
 # Create the test dataset commitment.
 # This operation records that the user with the above PRIVATE_KEY
 # has created the below named dataset.
 # Such commitments are used to validate completeness of a collection of user datasets
 # and mitigates Sybil attacks (https://en.wikipedia.org/wiki/Sybil_attack).
+# Dataset creation is idempotent.
 c2.create_dataset("TestDataset")
